14532b9304d51da8951dc19bd58428f3435e2fed
Aislan Max <marfebr@gmail.com>
2022-06-23 14:30:32 -0300

    Valida ramal

--- backend/src/portal_uft/src/portal_uft/content/person.py
+++ backend/src/portal_uft/src/portal_uft/content/person.py
@@ -6,6 +6,18 @@ from portal_uft import _
 from zope import schema
 from zope.interface import implementer
 
+import re
+
+
+def is_valid_email(value: str) -> bool:
+    """Check if email is valid"""
+    return value.endswith("@uft.edu.br")
+
+
+def is_valid_ramal(value: str) -> bool:
+    """Check if ramal is valid"""
+    return re.match(r"\d\d\d\d", value)
+
 
 class IPerson(Schema):
     """Schema of person profile"""
@@ -15,8 +27,14 @@ class IPerson(Schema):
         title=_("perso_description", default="Biography"), required=False
     )
 
-    email = Email(title=_("person_email", default="Email"), required=True)
-    extension = schema.TextLine(title=_("Extension"), required=False)
+    email = Email(
+        title=_("person_email", default="Email"),
+        required=True,
+        constraint=is_valid_email,
+    )
+    extension = schema.TextLine(
+        title=_("Extension"), required=False, constraint=is_valid_ramal
+    )
 
 
 @implementer(IPerson)
